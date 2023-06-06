706fed38b0a19c27e6875cb219e76cc7240d019d
CorentinAT <corentin.al2004@gmail.com>
2023-06-06 11:07:51 +0200

    import des drapeaux dans ImportDrapeaux.js

--- src/components/drapeaux/ListeDrapeaux.js
+++ src/components/drapeaux/ListeDrapeaux.js
@@ -1,7 +1,18 @@
 export default function ListeDrapeaux() {
+  const importAll = (r) => r.keys().map(r);
+  const images = importAll(require.context('../../img/drapeaux', false, /\.(png)$/));
+
   return (
     <div>
-      
+      {images.map((image, index) => {
+        const fileName = image.split('/').pop().split('.')[0].split('_').join(' ');
+        return (
+          <div key={index}>
+            <img src={`data:image/png;base64,${image}`} alt={`Image ${index + 1}`} />
+            <p>{fileName}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
