225d64b6d1add3c09daaf676dbc1ca1a0f9319f8
CorentinAT <corentin.al2004@gmail.com>
2023-06-06 19:15:32 +0200

    context cree pour les drapeaux utilises

---
+++ src/components/store/DrapeauxUtilisesProvider.js
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+
+import DrapeauxUtilisesContext from "./drapeaux-utilises-context";
+
+const drapeauxUtilisesParDefaut = {
+  drapeauxUtilises: [],
+}
+
+function DrapeauxUtilisesReducer(state, action) {
+  if(action.type==="AJOUTER_DRAPEAU") {
+    const updatedDrapeauxUtilises = [...state.drapeauxUtilises, action.drapeau.nom];
+    return {
+      ...state,
+      drapeauxUtilises: updatedDrapeauxUtilises
+    }
+  }
+  if(action.type==="RESET_DRAPEAUX_UTILISES") {
+    const updatedDrapeauxUtilises = [];
+    return {
+      ...state,
+      drapeauxUtilises: updatedDrapeauxUtilises
+    }
+  }
+}
+
+export default function DrapeauxUtilisesProvider(props) {
+  const [drapeauxUtilisesState, dispatchDrapeauxUtilises] = useReducer(DrapeauxUtilisesReducer, drapeauxUtilisesParDefaut);
+
+  function ajouterDrapeau(drapeau) {
+    dispatchDrapeauxUtilises({type: "AJOUTER_DRAPEAU", drapeau: drapeau});
+  }
+
+  function resetDrapeauxUtilises() {
+    dispatchDrapeauxUtilises({type: "RESET_DRAPEAUX_UTILISES"});
+  }
+
+  const drapeauxUtilisesContext = {
+    drapeauxUtilises: drapeauxUtilisesState.drapeauxUtilises,
+    ajouterDrapeau,
+    resetDrapeauxUtilises
+  }
+
+  return (
+    <DrapeauxUtilisesContext.Provider value={drapeauxUtilisesContext}>
+      {props.children}
+    </DrapeauxUtilisesContext.Provider>
+  );
+}
