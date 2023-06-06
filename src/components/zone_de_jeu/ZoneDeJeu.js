855c3cf1663e04b6db1dec4e2867f1d83e2a0011
CorentinAT <corentin.al2004@gmail.com>
2023-06-06 23:44:36 +0200

    pourcents descendent quand ils faut

--- src/components/zone_de_jeu/ZoneDeJeu.js
+++ src/components/zone_de_jeu/ZoneDeJeu.js
@@ -2,12 +2,14 @@ import { useState, useContext, useEffect } from "react";
 import { drapeaux } from "../../utils/ImportDrapeaux";
 import ZoneDentree from "./ZoneDentree";
 import DrapeauxUtilisesContext from "../store/drapeaux-utilises-context";
+import ResultatsContext from "../store/resultats-context";
 
 export default function ZoneDeJeu() {
   const [drapeauActuel, setDrapeauActuel] = useState(drapeaux[Math.floor(Math.random() * drapeaux.length)]);
   const [drapeauxRestants, setDrapeauxRestants] = useState([...drapeaux]);
 
-  const ctx = useContext(DrapeauxUtilisesContext);
+  const ctxDrapeauxUtilises = useContext(DrapeauxUtilisesContext);
+  const ctxResultats = useContext(ResultatsContext);
 
   function nettoyerChaine(chaine) {
     const chaineNettoyee = chaine.replace(/[^a-zA-Z]/g, '');
@@ -18,11 +20,14 @@ export default function ZoneDeJeu() {
     console.log(nettoyerChaine(drapeauEntre));
     let retour = false;
     if(nettoyerChaine(drapeauEntre).toLowerCase()===nettoyerChaine(drapeauActuel.nom).toLowerCase()) {
-      ctx.ajouterDrapeau(drapeauActuel);
+      ctxDrapeauxUtilises.ajouterDrapeau(drapeauActuel);
       setDrapeauxRestants(drapeauxRestants.filter(pays => {
         return pays.nom !== drapeauActuel.nom;
       }));
       retour = true;
+    } else {
+      ctxResultats.enleverPourcents((1/(ctxDrapeauxUtilises.drapeauxUtilises.length+1))*100);
+      console.log(ctxDrapeauxUtilises.drapeauxUtilises.length);
     }
     return retour;
   }
