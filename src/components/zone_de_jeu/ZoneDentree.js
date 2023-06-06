2b9b19f8a2f06c7ab3e90fd6210ff26481665d42
CorentinAT <corentin.al2004@gmail.com>
2023-06-06 17:03:34 +0200

    useState transformé en useRef pour la zone de texte

--- src/components/zone_de_jeu/ZoneDentree.js
+++ src/components/zone_de_jeu/ZoneDentree.js
@@ -1,19 +1,14 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 export default function ZoneDentree(props) {
-  const [drapeauEntre, setDrapeauEntre] = useState("");
   const inputRef = useRef(null);
-
-  function ecritDrapeauHandler(event) {
-    setDrapeauEntre(event.target.value);
-  }
 
   function envoyerRep(event) {
     event.preventDefault();
     console.log(drapeauEntre);
-    const juste = props.onEnvoi(drapeauEntre);
+    const juste = props.onEnvoi(inputRef.current.value);
     if(juste) {
-      setDrapeauEntre("");
+      inputRef.current.value = "";
     }
   }
 
@@ -24,7 +19,7 @@ export default function ZoneDentree(props) {
   return (
     <div className="flex flex-col gap-2">
       <form onSubmit={envoyerRep}>
-        <input type="text" className="border" onChange={ecritDrapeauHandler} value={drapeauEntre} ref={inputRef}/>
+        <input type="text" className="border" onChange={ecritDrapeauHandler} ref={inputRef}/>
         <input type="submit" className="border" value="Envoyer"/>
       </form>
     </div>
