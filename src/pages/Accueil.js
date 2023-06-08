cafa7d5c535bdb8cb293bb4321710837fcb1eaa0
CorentinAT <corentin.al2004@gmail.com>
2023-06-08 19:06:28 +0200

    routeur passé en hashrouteur pour github pages

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -2,8 +2,8 @@ export default function Accueil() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col">
       <h1>Choisir mode</h1>
-      <a href="/drapeaux_react/europe" className="border p-3">Europe</a>
-      <a href="/drapeaux_react/onu" className="border p-3">onu</a>
+      <a href="/drapeaux_react#/drapeaux_react/europe" className="border p-3">Europe</a>
+      <a href="/drapeaux_react#/drapeaux_react/onu" className="border p-3">onu</a>
     </div>
   );
 }
