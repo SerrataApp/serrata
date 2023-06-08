ca97333b3847e9d27e5ba0b0337f8e97f66c4e4e
CorentinAT <corentin.al2004@gmail.com>
2023-06-08 18:24:08 +0200

    routeur configuré différemment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import DrapeauxUtilisesProvider from './components/store/DrapeauxUtilisesProvider';
 import ResultatsProvider from './components/store/ResultatsProvider';
-import Router from "./routes";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Accueil from "./pages/Accueil";
+import Jeu from "./pages/Jeu";
+import { drapeauxEurope, drapeauxONU } from "./utils/ImportDrapeaux";
 
 export default function App() {
   return (
     <ResultatsProvider>
       <DrapeauxUtilisesProvider>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/drapeaux_react" replace />} />
+            <Route path="/drapeaux_react" element={<Accueil />} />
+            <Route path="/drapeaux_react/europe" element={<Jeu drapeaux={drapeauxEurope} />} />
+            <Route path="/drapeaux_react/onu" element={<Jeu drapeaux={drapeauxONU} />} />
+          </Routes>
+        </Router>
       </DrapeauxUtilisesProvider>
     </ResultatsProvider>
   );
