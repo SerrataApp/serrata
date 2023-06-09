7f5c1d70a7ba03044a3ca4572d7ccb371efdba1b
CorentinAT <corentin.al2004@gmail.com>
2023-06-09 13:06:22 +0200

    ajout du nombre d'indices dans le contexte

--- src/components/store/resultats-context.js
+++ src/components/store/resultats-context.js
@@ -2,9 +2,11 @@ import { createContext } from "react"
 
 const ResultatsContext = createContext({
   erreurs: 0,
+  indices: 0,
   temps: 0,
   estFini: false,
   ajouterErreur: () => {},
+  ajouterIndice: () => {},
   definirTemps: temps => {},
   finir: () => {}
 })
