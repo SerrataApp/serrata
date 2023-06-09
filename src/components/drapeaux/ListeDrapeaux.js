a73f8ef7d8062bf3ddb64fcc8264da3edafa5ddf
CorentinAT <corentin.al2004@gmail.com>
2023-06-09 08:30:34 +0200

    justify around pour liste drapeaux

--- src/components/drapeaux/ListeDrapeaux.js
+++ src/components/drapeaux/ListeDrapeaux.js
@@ -2,7 +2,7 @@ import PetitDrapeau from "./PetitDrapeau";
 
 export default function ListeDrapeaux(props) {
     return (
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap justify-around">
       {props.drapeaux.map(drapeau => {
         return (
           <PetitDrapeau key={drapeau.nom} drapeau={drapeau}/>
