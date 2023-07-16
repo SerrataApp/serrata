a693664b6fe4e985d860d9f9921875bd27e3c2eb
CorentinAT <corentin.al2004@gmail.com>
2023-07-16 13:40:43 +0200

    ajout mode Afrique

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ResultatsProvider from './components/store/ResultatsProvider';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Accueil from "./pages/Accueil";
 import Jeu from "./pages/Jeu";
-import { drapeauxEurope, drapeauxONU } from "./utils/ImportDrapeaux";
+import { drapeauxAfrique, drapeauxEurope, drapeauxONU } from "./utils/ImportDrapeaux";
 import Scores from './pages/Scores';
 
 export default function App() {
@@ -13,8 +13,9 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Accueil />} />
-            <Route path="/europe" element={<Jeu drapeaux={drapeauxEurope}/>} />
-            <Route path="/onu" element={<Jeu drapeaux={drapeauxONU}/>} />
+            <Route path="/europe" element={<Jeu drapeaux={drapeauxEurope} titre="Europe"/>} />
+            <Route path="/onu" element={<Jeu drapeaux={drapeauxONU}/>} titre="ONU"/>
+            <Route path="/afrique" element={<Jeu drapeaux={drapeauxAfrique} titre="Afrique"/>} />
             <Route path="/scores" element={<Scores/>} />
           </Routes>
         </BrowserRouter>
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,6 +3,7 @@ export default function Accueil() {
     <div className="w-screen h-screen flex justify-center items-center flex-col">
       <h1>Choisir mode</h1>
       <a href="/europe" className="border p-3">Europe</a>
+      <a href="/afrique" className="border p-3">Afrique</a>
       <a href="/onu" className="border p-3">ONU</a>
       <a href="/scores" className="border p-3">Scores</a>
     </div>
