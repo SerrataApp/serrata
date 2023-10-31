15426374b3eafa6dc2986d30fe6ff60b3d4031f5
CorentinAT <corentin.al2004@gmail.com>
2023-10-31 13:30:35 +0100

    affichage temps propre

--- src/components/profil/ResumePartie.js
+++ src/components/profil/ResumePartie.js
@@ -1,7 +1,9 @@
 export default function ResumePartie(props) {
+  const minutes = Math.floor(props.temps/60);
+  const secondes = props.temps%60;
   return (
     <tr>
-      <td className="p-1 border">{props.temps}</td>
+      <td className="p-1 border">{minutes < 10 ? "0" + minutes : minutes}:{secondes < 10 ? "0" + secondes : secondes}</td>
       <td className="p-1 border">{props.erreurs}</td>
       <td className="p-1 border">{props.date}</td>
     </tr>
