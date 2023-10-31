9d2a260fd177bb753f2c9d9d7ee437c28ac3c8cb
CorentinAT <corentin.al2004@gmail.com>
2023-10-31 13:39:17 +0100

    changement accueil

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,10 +1,12 @@
 export default function Accueil() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-1">
-      <h1>Choisir mode</h1>
+      <h1 className="font-bold">Serrata</h1>
+      <h2>Choisir mode</h2>
       <a href="/europe" className="border p-3">Europe</a>
       <a href="/afrique" className="border p-3">Afrique</a>
       <a href="/monde" className="border p-3">Monde</a>
+      <h2>Autres</h2>
       <a href="/scores" className="border p-3">Scores</a>
       <a href="/profil" className="border p-3">Profil</a>
     </div>
