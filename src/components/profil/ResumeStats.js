333147102d91b58bea3885557df5d3bfaaa033df
CorentinAT <corentin.al2004@gmail.com>
2023-11-01 13:33:35 +0100

    affichage temps changé de component

--- src/components/profil/ResumeStats.js
+++ src/components/profil/ResumeStats.js
@@ -1,11 +1,15 @@
 import Stat from "./Stat";
 
 export default function ResumeStats(props) {
+  const minutes = Math.floor(props.stats.temps_moyen/60);
+  const secondes = Math.round(props.stats.temps_moyen%60);
+  const temps_moyen = `${minutes < 10 ? "0" + minutes : minutes}:${secondes < 10 ? "0" + secondes : secondes}`;
+
   return (
     <div>
       <Stat key="parties_lancees" nom="Parties lancées" valeur={props.stats.parties_lancees}/>
       <Stat key="parties_finies" nom="Parties terminées" valeur={props.stats.parties_finies}/>
-      <Stat key="temps_moyen" nom="Temps moyen" valeur={props.stats.temps_moyen}/>
+      <Stat key="temps_moyen" nom="Temps moyen" valeur={temps_moyen}/>
     </div>
   );
 }
--- src/components/profil/Stat.js
+++ src/components/profil/Stat.js
@@ -1,16 +1,8 @@
 export default function Stat(props) {
-  let valeur = props.valeur;
-
-  if(props.nom==="Temps moyen") {
-    const minutes = Math.floor(props.valeur/60);
-    const secondes = Math.round(props.valeur%60);
-    valeur = `${minutes < 10 ? "0" + minutes : minutes}:${secondes < 10 ? "0" + secondes : secondes}`;
-  }
-
   return (
     <div>
       <h2 className="font-semibold">{props.nom}</h2>
-      <span>{valeur}</span>
+      <span>{props.valeur}</span>
     </div>
   );
 }
