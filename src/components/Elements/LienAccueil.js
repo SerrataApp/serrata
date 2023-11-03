465a2c03ab2481cf0beed4f4472d37b09a085f5f
CorentinAT <corentin.al2004@gmail.com>
2023-11-03 23:40:57 +0100

    changement scale d'un truc

--- src/components/Elements/LienAccueil.js
+++ src/components/Elements/LienAccueil.js
@@ -14,7 +14,7 @@ export default function LienAccueil(props) {
   return(
     <a href={props.href} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} className="overflow-hidden rounded h-44 flex justify-center items-center border">
       <p className="text-3xl absolute z-10 text-white" style={{ textShadow: "0px 0px 5px rgba(0, 0, 0, 0.8)" }}>{props.texte}</p>
-      <img src={props.img} alt={props.texte} className={`w-full blur-sm transition-all duration-200 ${isHover && "scale-110"}`}/>
+      <img src={props.img} alt={props.texte} className={`w-full blur-sm transition-all duration-200 scale-110 sc ${isHover && "scale-125"}`}/>
     </a>
   );
 }
