13c4c005cc264a73c5ca36022256df7e622bede6
CorentinAT <corentin.al2004@gmail.com>
2023-11-03 16:36:19 +0100

    header avance

--- src/components/Elements/Header.js
+++ src/components/Elements/Header.js
@@ -1,7 +1,11 @@
 export default function Header() {
   return(
-    <div className="w-screen max-w-screen bg-secondary p-4">
+    <div className="w-screen max-w-screen bg-secondary p-4 flex justify-between">
       <a href="/"><h1 className="select-none">Serrata</h1></a>
+      <div>
+        <a href="/scores" className="text-blue-700 hover:underline">Scores</a>
+        <a href="/profil" className="ml-5 text-blue-700 hover:underline">Profil</a>
+      </div>
     </div>
   );
 }
