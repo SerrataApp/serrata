2bc4c03f8d4723d266671979473d5146a8cecaaf
CorentinAT <corentin.al2004@gmail.com>
2023-11-24 16:17:18 +0100

    gestion erreur connexion

--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -1,12 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Page from "./Page";
 
 export default function Connexion() {
+  const [erreur, setErreur] = useState();
+
   const inputPseudo = useRef();
   const inputMdp = useRef();
 
   function onSubmitHandler(e) {
     e.preventDefault();
+    setErreur(null);
     const url = "http://127.0.0.1:8000/token";
     const formData = new URLSearchParams();
     formData.append("username", inputPseudo.current.value);
@@ -20,9 +23,16 @@ export default function Connexion() {
       },
       body: formData,
     })
-    .then(response => response.json())
-    .then(data => {
-      window.localStorage.setItem("token", data.access_token);
+    .then(response => {
+      response.json()
+      .then(data => {
+        switch(response.status) {
+          case 200: window.localStorage.setItem("token", data.access_token); break;
+          case 401: setErreur(data.detail); break;
+          case 422: setErreur(`Champ manquant : ${data.detail[0].loc[1]}`); break;
+          default: setErreur("Erreur, veuillez réessayer");
+        }
+      });
     })
   }
 
@@ -39,6 +49,7 @@ export default function Connexion() {
             <input type="password" className="border" ref={inputMdp}/>
           </label>
           <input type="submit" value="Se connecter" className="bg-white border p-2"/>
+          {erreur && <p className="text-red-500">{erreur}</p>}
         </form>
       </div>
     </Page>
