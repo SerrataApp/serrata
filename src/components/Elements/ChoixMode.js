b5e872fedec24219010822a4b3e9e57034e52090
CorentinAT <corentin.al2004@gmail.com>
2023-11-30 23:34:55 +0100

    choixmode responsive

--- src/components/Elements/ChoixMode.js
+++ src/components/Elements/ChoixMode.js
@@ -4,13 +4,13 @@ export default function ChoixMode(props) {
   }
 
   return (
-    <div className="join mb-5">
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="1" aria-label="Europe" checked={props.modeSelect==="1"}/>
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="2" aria-label="Afrique" checked={props.modeSelect==="2"} />
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="3" aria-label="Asie" checked={props.modeSelect==="3"} />
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="4" aria-label="Amérique" checked={props.modeSelect==="4"} />
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="5" aria-label="Océanie" checked={props.modeSelect==="5"} />
-      <input onClick={onSelectHandler} className="join-item btn btn-square w-fit px-4" type="radio" name="choixMode" value="0" aria-label="Monde" checked={props.modeSelect==="0"} />
+    <div className="join join-vertical md:join-horizontal mb-5 w-full flex items-center justify-center">
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="1" aria-label="Europe" checked={props.modeSelect==="1"}/>
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="2" aria-label="Afrique" checked={props.modeSelect==="2"} />
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="3" aria-label="Asie" checked={props.modeSelect==="3"} />
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="4" aria-label="Amérique" checked={props.modeSelect==="4"} />
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="5" aria-label="Océanie" checked={props.modeSelect==="5"} />
+      <input onClick={onSelectHandler} className="join-item btn btn-square w-8/12 md:w-fit px-4" type="radio" name="choixMode" value="0" aria-label="Monde" checked={props.modeSelect==="0"} />
     </div>
   );
 }
