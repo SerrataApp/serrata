825586436cc1ff0975f9eafde330fb224ace83ed
CorentinAT <corentin.al2004@gmail.com>
2023-12-05 12:02:07 +0100

    padding right sur footer

--- src/components/Elements/Footer.js
+++ src/components/Elements/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-full bg-gray-100 py-2 flex justify-end gap-4">
+    <div className="w-full bg-gray-100 py-2 flex justify-end gap-4 pr-5">
       <a target="_blank" href="https://github.com/corentinAT/serrata" className="hover:underline">Github</a>
       <a href="/mentionslegales" className="hover:underline">Mentions légales</a>
       <a href="/cgu" className="hover:underline">Conditions générales d'utilisation</a>
