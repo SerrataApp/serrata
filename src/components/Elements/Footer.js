9bbe0f52ab529618e743a9a24c7f225d4c926227
CorentinAT <corentin.al2004@gmail.com>
2023-12-11 12:56:52 +0100

    conditions etc devient CGU sur petit ecran

--- src/components/Elements/Footer.js
+++ src/components/Elements/Footer.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function Footer() {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="w-full bg-gray-100 py-2 flex justify-end gap-4 pr-5">
-      <a target="_blank" href="https://github.com/corentinAT/serrata" className="hover:underline">Github</a>
-      <a href="/mentionslegales" className="hover:underline">Mentions légales</a>
-      <a href="/cgu" className="hover:underline">Conditions générales d'utilisation</a>
+    <div className="w-full bg-gray-100 py-2 flex justify-end items-center gap-3 md:gap-4 pr-5">
+      <a target="_blank" href="https://github.com/corentinAT/serrata" className="hover:underline text-center w-1/3 md:w-auto">Github</a>
+      <a href="/mentionslegales" className="hover:underline text-center w-1/3 md:w-auto">Mentions légales</a>
+      <a href="/cgu" className="hover:underline text-center w-1/3 md:w-auto">{innerWidth<768?"CGU":"Conditions générales d'utilisation"}</a>
     </div>
   );
 }
