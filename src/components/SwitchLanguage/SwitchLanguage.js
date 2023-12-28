eca0c9a5c88b938500bd4fb56655cfa8d398b3e7
CorentinAT <corentin.al2004@gmail.com>
2023-12-28 22:31:48 +0100

    fix bug image petit drapeau merci xavier

--- src/components/SwitchLanguage/SwitchLanguage.js
+++ src/components/SwitchLanguage/SwitchLanguage.js
@@ -1,12 +1,14 @@
 import { useContext, useState } from "react";
-import france from "../../img/drapeaux/france.png";
-import royaumeuni from "../../img/drapeaux/royaumeuni.png";
+// import france from "../../img/drapeaux/france.png";
+// import royaumeuni from "../../img/drapeaux/royaumeuni.png";
 import LanguageContext from "../store/language-context";
 import SelectionLangue from "./SelectionLangue";
 
 export default function SwitchLanguage() {
   const [showListeLangue, setShowListeLangue] = useState(false);
 
+  const france = require("../../img/drapeaux/france.png");
+  const royaumeuni = require("../../img/drapeaux/royaumeuni.png");
   const ctxLanguage = useContext(LanguageContext);
   const drapeaux = {
     fr: france,
@@ -24,9 +26,9 @@ export default function SwitchLanguage() {
   }
 
   return (
-    <div className="relative">
+    <div className="relative z-30">
       <div className="flex gap-1 hover:cursor-pointer" onClick={toggleListeLangues}>
-        <img src={image} className="h-4"/>
+        <img src={image} className="h-4" alt="language"/>
         <i className={`fa fa-chevron-up transition-all duration-150 ${!showListeLangue?"fa-flip-vertical":""}`}></i>
       </div>
       {showListeLangue && <SelectionLangue drapeaux={drapeaux} closeDiv={toggleListeLangues}/>}
