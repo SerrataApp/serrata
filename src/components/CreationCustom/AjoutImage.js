b0b57db7e0d2085db955ce7822e30799d11d9eac
CorentinAT <corentin.al2004@gmail.com>
2024-02-10 13:43:24 +0100

    structure page creermode

--- src/components/CreationCustom/AjoutImage.js
+++ src/components/CreationCustom/AjoutImage.js
@@ -1,7 +1,26 @@
-export default function AjoutImage() {
+import { useRef } from "react";
+import Input from "../Elements/Input";
+
+export default function AjoutImage(props) {
+  const nomRef = useRef();
+  const alias1Ref = useRef();
+  const alias2Ref = useRef();
+
+  function supprimer() {
+    props.supprimer(props.index);
+  }
+
+  function modifierAttibut(attribut, ref) {
+    props.modifierAttibut(props.index, attribut, ref.current.value)
+  }
+
   return (
-    <div className="w-full bg-gray-100 rounded p-1 flex">
-      
+    <div className="w-full bg-gray-100 rounded p-1 flex flex-col gap-2 md:gap-0 md:flex-row justify-between">
+      <Input label="Image" type="file" obligatoire={true} class="border-none" accept=".png, .jpg, .jpeg"/>
+      <Input onChange={() => {modifierAttibut("nom", nomRef)}} reference={nomRef} label="Nom principal" type="text" obligatoire={true} autofocus={true}/>
+      <Input onChange={() => {modifierAttibut("alias1", alias1Ref)}} reference={alias1Ref} label="Alias 1" type="text"/>
+      <Input onChange={() => {modifierAttibut("alias2", alias2Ref)}} reference={alias2Ref} label="Alias 2" type="text"/>
+      <button onClick={supprimer} className="bg-red-500 hover:bg-red-400 rounded transition-bg duration-150 h-fit p-2 self-center">Supprimer</button>
     </div>
   );
 }
--- src/components/CreationCustom/CreationCustom.js
+++ src/components/CreationCustom/CreationCustom.js
@@ -1,18 +1,51 @@
+import { useState } from "react";
 import Page from "../../pages/Page";
 import AjoutImage from "./AjoutImage";
 import InfosMode from "./InfosMode";
 
 export default function CreationCustom() {
+  const [listeImages, setListeImages] = useState([]);
+
+  function ajouterImage() {
+    let nvKey = 0;
+    if(listeImages.length > 0) {
+      nvKey = listeImages.reduce((max, item) => (item.key > max ? item.key : max), -1) + 1;
+    }
+    setListeImages([{key: nvKey}, ...listeImages]);
+  }
+
+  function modifierAttibut(index, attribut, valeur) {
+    const tmpListe = [...listeImages];
+    tmpListe[index] = {
+      ...tmpListe[index],
+      [attribut]: valeur
+    };
+    setListeImages(tmpListe);
+  }
+
+  function supprimerImage(index) {
+    setListeImages(listeImages.filter((_, i) => i !== index))
+  }
+
+  const divsImages = listeImages.map((image, index) => <AjoutImage key={image.key} index={index} supprimer={supprimerImage} modifierAttibut={modifierAttibut}/>)
+
   return (
     <Page titre="Créer un mode de jeu">
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center mb-6">
         <div className="w-[80%] md:w-[70%] lg:w-[60%] flex flex-col gap-3">
           <InfosMode/>
           <div className="flex justify-between">
-            <span>Total : 12</span>
-            <button className="border border-black rounded">+</button>
+            <span>Total : {divsImages.length}</span>
+            <button onClick={ajouterImage} className="border border-black rounded w-5 h-5 flex justify-center items-center hover:shadow-md transition-shadow duration-150">+</button>
           </div>
-          <AjoutImage/>
+          {divsImages}
+        </div>
+      </div>
+      <div className="fixed w-screen flex bottom-[75px] justify-center">
+        <div className="flex gap-4 bg-white rounded-xl border border-black p-3 shadow-lg">
+          <button className="p-2 rounded shadow-md bg-red-500 hover:bg-red-400 transition-bg duration-150">Annuler</button>
+          <button className="p-2 rounded shadow-md bg-blue-500 hover:bg-blue-400 transition-bg duration-150">Réinitialiser</button>
+          <button className="p-2 rounded shadow-md bg-green-500 hover:bg-green-400 transition-bg duration-150">Valider</button>
         </div>
       </div>
     </Page>
